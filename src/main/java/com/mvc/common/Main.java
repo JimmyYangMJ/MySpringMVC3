c0f5b5e23c652900d6805024583ac1b2805178cd
YMj1998 <2237895205@qq.com>
2019-09-21 18:41:32 +0800

    first commmit init project

---
+++ src/main/java/com/mvc/common/Main.java
@@ -0,0 +1,9 @@
+package com.mvc.common;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ResponseCode.SUCCESS.getCode());
+        System.out.println(ResponseCode.SUCCESS.getDesc());
+
+    }
+}
